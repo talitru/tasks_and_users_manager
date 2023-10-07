db370d36e1b6764ceb589be8f6bcb8c6531c4465
talitru <talitru123@gmail.com>
2023-10-08 00:30:30 +0300

    task+comment without dtos and mapper

--- src/main/java/com/shufersalOnline/tasksAndUsersApi/controller/CommentController.java
+++ src/main/java/com/shufersalOnline/tasksAndUsersApi/controller/CommentController.java
@@ -1,7 +1,42 @@
 package com.shufersalOnline.tasksAndUsersApi.controller;
 
+import com.shufersalOnline.tasksAndUsersApi.entity.Comment;
+import com.shufersalOnline.tasksAndUsersApi.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService=commentService;
+    }
+
+    @GetMapping("/comments")
+    public ResponseEntity<List<Comment>> getAllCommentsO(){
+
+        List<Comment> comments= commentService.getAllComments();
+        return ResponseEntity.ok(comments);
+    }
+
+    @GetMapping("/users/{userId}/comments")
+    public ResponseEntity<List<Comment>> getAllCommentsOfUser(@PathVariable Long userId){
+
+        List<Comment> comments= commentService.getAllCommentsOfUser(userId);
+        return ResponseEntity.ok(comments);
+    }
+
+    @GetMapping("/users/{userId}/tasks/{taskId}/comments")
+    public ResponseEntity<List<Comment>> getAllCommentsOfUserTask(@PathVariable Long userId,
+                                                                   @PathVariable Long taskId){
+
+        List<Comment> comments= commentService.getAllCommentsOfUserTask(userId,taskId);
+        return ResponseEntity.ok(comments);
+    }
 }
